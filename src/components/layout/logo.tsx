2f3b231d82df2f902eefd62b5b59195d7bfe6fae
Z nation Man <znationman217@gmail.com>
2025-07-10 13:41:20 +0000

    it is still the same lets figure out how the logo will be implemented to

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -3,6 +3,8 @@
 
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { BookOpenText } from "lucide-react"; // Using a standard, reliable icon for diagnostics
+import { siteConfig } from "@/config/site";
 
 interface LogoProps {
   className?: string;
@@ -14,24 +16,15 @@ export function Logo({ className = "" }: LogoProps) {
 
   return (
     <Link href="/" className={cn("group flex items-center gap-2", className)}>
-      {/* User-provided SVG, formatted for JSX */}
-      <svg
-        viewBox="0 15 150 250"
-        xmlns="http://www.w3.org/2000/svg"
-        role="img"
-        aria-label="ABYLANG Logo"
-        preserveAspectRatio="xMidYMid meet"
-        className="logo-svg"
-      >
-        <path
-          fillRule="evenodd"
-          fill="currentColor"
-          d="M 35,265 L 115,265 L 115,80 C 125,75 125,60 115,55 L 115,30 A 40,40 0 0 0 35,30 L 35,55 C 25,60 25,75 35,80 L 35,265 Z M 71,36 a 3.5,3.5 0 1 1 -7,0 a 3.5,3.5 0 1 1 7,0 Z M 86,36 a 3.5,3.5 0 1 1 -7,0 a 3.5,3.5 0 1 1 7,0 Z M 75,48 a 4,4 0 1 1 -8,0 a 4,4 0 1 1 8,0 Z M 62,48 a 4,4 0 1 1 -8,0 a 4,4 0 1 1 8,0 Z M 91,180 Q 75,180 75,160 Q 75,140 91,140 L 108,140 C 100,130 105,118 114,118 C 123,118 128,130 120,140 L 91,140 Z M 108,150 L 108,170 L 91,170 Q 82,170 82,160 Q 82,150 91,150 L 108,150 Z"
-        />
-      </svg>
+      {/* 
+        Diagnostic Step: Using a standard Lucide icon instead of a custom SVG.
+        This helps determine if the issue is with the SVG data or the component's structure and styling.
+      */}
+      <BookOpenText className="logo-svg" />
+
       {showText && (
-        <span className="logo-text text-xl font-semibold text-primary group-[.footer-logo]:text-background">
-          ABYLANG
+        <span className="logo-text text-xl font-semibold">
+          {siteConfig.name}
         </span>
       )}
     </Link>
