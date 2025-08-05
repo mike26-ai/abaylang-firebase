3d7ed56b9ee54794d4f074d41781c798e2c5846c
Z nation Man <znationman217@gmail.com>
2025-08-05 13:32:51 +0000

    please take a look at the change i manually made in the logo code sectio

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -13,40 +13,29 @@ interface LogoProps {
 export function Logo({ className }: LogoProps) {
   return (
     <Link href="/" className={cn("flex items-center group", className)}>
-      {/* This is the new, confirmed SVG code for the "A" river logo */}
+      {/* This is the new SVG code with the height increased */}
       <svg
-        viewBox="0 0 140 140"
-        className={cn("h-12 w-auto", className)}
+        viewBox="0 0 100 130"
+        // The height has been changed from h-10 to h-14 to make it taller
+        className={cn("h-14 w-auto", className)} 
         aria-label="Abylang Logo"
       >
-        <defs>
-          <clipPath id="riverClip">
-            <path d="M65,0 L35,115 H55 L85,0 H65 Z" />
-          </clipPath>
-        </defs>
-        
-        {/* Main 'A' shape */}
+        {/* Obelisk Shape with Cutouts */}
         <path
-          d="M70,5 L10,115 H30 L50,60 H90 L110,115 H130 L70,5 Z"
-          fill="#2D3748"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          fill="hsl(var(--foreground))"
+          d="M38.5,5.1 C39.5,2.3 42.5,0 45.6,0 L54.4,0 C57.5,0 60.5,2.3 61.5,5.1 L75,45 L75,115 L25,115 L25,45 L38.5,5.1 Z M45,20 a3,3 0 1,0 6,0 a3,3 0 1,0 -6,0 Z M55,20 a3,3 0 1,0 6,0 a3,3 0 1,0 -6,0 Z M45,30 a3,3 0 1,0 6,0 a3,3 0 1,0 -6,0 Z M55,30 a3,3 0 1,0 6,0 a3,3 0 1,0 -6,0 Z M43,85 C43,93 49,97 53,97 C57,97 60,94 60,91 C60,88 56,86 53,86 L53,73 C58,73 63,70 63,65 C63,60 58,58 53,58 C48,58 43,60 43,65 C43,70 48,73 53,73 L53,83 C51,83 46,83 46,85 L43,85 Z"
         />
-        
-        {/* River Shape (Blue Nile) */}
-        <g clipPath="url(#riverClip)">
-          <path
-            d="M-10,50 Q20,80 50,80 T110,70 L110,90 Q80,100 50,100 T-10,70 Z"
-            fill="#4299E1"
-          />
-        </g>
-        
-        {/* The word "ABYLANG" */}
+
+        {/* The word "ABYLANG" below the obelisk */}
         <text
-          x="70"
-          y="135"
+          x="50"
+          y="128"
           fontFamily="sans-serif"
-          fontSize="18"
+          fontSize="14"
           fontWeight="bold"
-          fill="#D97706"
+          fill="hsl(var(--primary))"
           textAnchor="middle"
         >
           ABYLANG
