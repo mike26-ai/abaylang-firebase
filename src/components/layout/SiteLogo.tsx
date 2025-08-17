bd44998816c944f33e66bdae9705283f9ba418c6
Z nation Man <znationman217@gmail.com>
2025-08-17 00:18:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/SiteLogo.tsx
+++ src/components/layout/SiteLogo.tsx
@@ -1,6 +1,5 @@
 // File: src/components/layout/SiteLogo.tsx
 import Link from 'next/link';
-// @ts-ignore - This will work once the SVG is moved and the build process is configured for SVG imports
 import LogoSvg from '@/components/icons/logo.svg'; // Import the SVG as a component
 
 export function SiteLogo() {
--- src/next.config.ts
+++ src/next.config.ts
@@ -22,11 +22,30 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
+  webpack(config, { isServer }) {
+    // Grab the existing rule that handles images
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      // Re-add the existing rule, but modify it to ignore SVGs
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not ?? []), /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    // Modify the original rule to ignore SVGs
+    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
