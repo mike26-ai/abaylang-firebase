fad1bc84aa37d92b0f02b4492d7ecfedf03958cf
Your Name <you@example.com>
2025-08-19 08:26:20 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/next.config.ts
+++ src/next.config.ts
@@ -24,33 +24,11 @@ const nextConfig: NextConfig = {
     ],
   },
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      typeof rule === 'object' && rule.test?.test?.('.svg')
-    );
-
-    config.module.rules.push(
-      // Re-add the existing rule, but modify it to ignore SVGs when
-      // they are imported from files ending in `.js` or `.ts` (or .jsx, .tsx).
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports into React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      }
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have a new rule that handles it
-    if (fileLoaderRule && typeof fileLoaderRule === 'object') {
-        fileLoaderRule.exclude = /\.svg$/i;
-    }
-
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
 
     return config;
   },
