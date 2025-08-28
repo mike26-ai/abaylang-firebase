8a45dcfeb3e2cffac7bee39fbb80f85b9ac4941d
Z nation Man <znationman217@gmail.com>
2025-08-28 17:49:05 +0000

    Please revert the recent changes related to the logo implementation (the

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
