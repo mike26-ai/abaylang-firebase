bbfcadbcc3fb0720ac1481a6bc890d96ef00275c
Z nation Man <znationman217@gmail.com>
2025-09-08 08:48:27 +0000

    It is still the same what now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from 'next';
 import React from "react";
+import Script from 'next/script'; // Import the Script component
 import { Lato, Lora } from "next/font/google";
 import "./globals.css";
 import { FirebaseProvider } from "@/components/providers/firebase-provider";
@@ -46,6 +47,8 @@ export default function RootLayout({
           <ConditionalLayoutWrapper>{children}</ConditionalLayoutWrapper>
           <Toaster />
         </FirebaseProvider>
+        {/* Add the Paddle.js script here. It will load asynchronously. */}
+        <Script src="https://cdn.paddle.com/paddle/paddle.js" />
       </body>
     </html>
   );
