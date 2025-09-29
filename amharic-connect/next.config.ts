70c51e3e3955edf1bc0feda766a09a9132364b31
Z nation Man <znationman217@gmail.com>
2025-09-29 14:20:31 +0000

    A 404: This page could not be found in your case usually means one of th

---
+++ amharic-connect/next.config.ts
@@ -0,0 +1,42 @@
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // This experimental block is necessary for Firebase Studio to work correctly.
+  experimental: {
+    allowedDevOrigins: [
+      'https://*.google.com',
+      'https://*.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
